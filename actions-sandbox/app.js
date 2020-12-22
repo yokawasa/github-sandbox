e7d307a58e24c601b31c4cb63895ea57e3053dbb
yokawasa <yokawasa@gmail.com>
2020-12-23 00:51:53 +0900

    updated

--- actions-sandbox/app.js
+++
@@ -1,7 +0,0 @@
-var app = require('express')()
-
-app.get('/',function(req,res) {
-  res.send("Hello World")
-})
-
-app.listen(3000)
--- actions-sandbox/go/calculator.go
+++
@@ -1,9 +0,0 @@
-package calculator
-
-func add(a int, b int) int {
-  return a + b
-}
-
-func subtract(a int, b int) int {
-  return a - b
-}
